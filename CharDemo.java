ae022a9c320740ede37311f8accccf98f9b22453
Kimberly <44447847+iamkimbert@users.noreply.github.com>
2023-10-23 19:57:45 -0400

    Characters
    In Java, the data type used to store characters is char. Java uses Unicode to represent characters. Unicode is a fully international character set that can represent all of the characters found in all human languages. It is a unification of dozens of character sets, such as Latin, greek, arabic, Cyrillic, Hebrew, Katakana, Hangul, and many more.

---
+++ CharDemo.java
@@ -0,0 +1,17 @@
+/*
+	Demonstrate char data type.
+	Name this program CharDemo.java
+*/
+
+class CharDemo {
+	public static void main(String args[]) {
+		char ch1, ch2;
+		
+		ch1 = 88; // code for X
+			ch2 = 'Y';
+		
+		System.out.print("ch1 and ch2: ");
+		System.out.println(ch1 +  " " + ch2); 
+	}
+}
+
