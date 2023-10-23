5020713de7020b008d2d3e8fe4f858bcd667af6a
Kimberly <44447847+iamkimbert@users.noreply.github.com>
2023-10-23 19:34:04 -0400

    Long
    Long is a signed 64-bit type and is useful for those occasions where an int type is not large enough to hold the desired value. The range of a long is quite large. This makes it useful when big, whole numbers are needed. For example, here is a program that computes the number of miles that light will travel in a specified number of days.

---
+++ Light.java
@@ -0,0 +1,28 @@
+
+/*
+	Computes the number of miles that light will travel in a specified number of days
+	This program will be called "Light.java"
+*/
+
+// Compute distance light travels using long varables. 
+class Light {
+	public static void main(String args[]){
+		int lightspeed;
+		long days;
+		long seconds;
+		long distance;
+		
+		// approximate speed of light in miles per second
+		lightspeed = 186000;
+		
+		days = 1000; // specify number of dats here
+		seconds = days * 24 * 60 * 60; // convert to seconds
+		
+		distance = lightspeed * seconds; // compute distance
+		
+		System.out.print("In " + days);
+		System.out.print(" days light will travel about ");
+		System.out.println("distance + miles.");
+	}
+}
+
